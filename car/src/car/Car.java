798351065a31d50dee44def1790607d6a0a979d2
Kevin Bell <bellKevin@pm.me>
2022-06-03 02:39:16 +0000

    Update Car.java

--- car/src/car/Car.java
+++ car/src/car/Car.java
@@ -1,8 +1,32 @@
 package car;
 
-public class Car {
+public class carDriver {
 
-    public static void main(String[] args) {
+    private String make;
+    private int year;
+    private String color;
 
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Car makeCopy() {
+        Car car = new Car();
+        car.make = this.make;
+        car.year = this.year;
+        car.color = this.color;
+        return car;
+    }
+
+    public void display() {
+        System.out.printf("make= %s\nyear= %s\ncolor= %s\n", this.make, this.year, this.color);
     }
 }
